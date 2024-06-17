a44d1a4bbd04651ebef332283a60073f72f52557
Vicente La Spina <72106203+VicenteLaSpinaa@users.noreply.github.com>
2024-06-17 11:02:17 -0400

    Update Validarint(entero).py
    Lo hice a la rápida a si q no tiene sentido la sintaxis xd

--- Validarint(entero).py
+++ Validarint(entero).py
@@ -10,3 +10,13 @@ def solicitar_entero(mensaje):
 # Uso de la función
 edad = solicitar_entero("Por favor, ingrese su edad: ")
 print(f"Edad ingresada: {edad}")
+
+def menu_lal(menu):
+    while True:
+        try:
+            menu = input("Escriba 1 para terminar el programa")
+            if menu == 1:
+                break
+        except ValueError:
+            print("Opción no válida, por favor ingrese 1")
+            
